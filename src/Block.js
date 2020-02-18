d580bf8dd25206914431ea2349681284ef3e6b6a
Titux Metal <github@lgdweb.fr>
2020-02-19 00:07:17 +0100

    Update mineBlock method; create a hash field

--- src/Block.js
+++ src/Block.js
@@ -1,4 +1,5 @@
 const { GENESIS_DATA } = require('./config')
+const cryptoHash = require('./cryptoHash')
 
 class Block {
   constructor ({ timestamp, lastHash, hash, data }) {
@@ -15,9 +16,14 @@ class Block {
   }
 
   static mineBlock ({ lastBlock, data }) {
+    const timestamp = Date.now()
+    const lastHash = lastBlock.hash
+    const hash = cryptoHash(timestamp, lastHash, data)
+
     return new Block({
-      timestamp: Date.now(),
-      lastHash: lastBlock.hash,
+      timestamp,
+      lastHash,
+      hash,
       data
     })
   }
