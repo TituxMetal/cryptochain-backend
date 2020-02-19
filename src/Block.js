ca2b599b6523709fe3a481bf2dbbd64dd4a23e60
Titux Metal <github@lgdweb.fr>
2020-02-19 15:12:16 +0100

    Update Block class; use hexToBinary in mineBlock method

--- src/Block.js
+++ src/Block.js
@@ -1,3 +1,5 @@
+const hexToBinary = require('hex-to-binary')
+
 const { GENESIS_DATA, MINE_RATE } = require('./config')
 const cryptoHash = require('./cryptoHash')
 
@@ -31,7 +33,9 @@ class Block {
         timestamp
       })
       hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty)
-    } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty))
+    } while (
+      hexToBinary(hash).substring(0, difficulty) !== '0'.repeat(difficulty)
+    )
 
     return new Block({
       timestamp,
