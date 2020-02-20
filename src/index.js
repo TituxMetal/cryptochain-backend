ed965557944f4dcff0f32dbbc7808609729453f4
Titux Metal <github@lgdweb.fr>
2020-02-20 09:33:54 +0100

    Update index.js file; add express server and a get route for /api/blocks

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('cryptochain backend')
+const express = require('express')
+
+const { Blockchain } = require('./blockchain')
+const { port } = require('./config')
+
+const app = express()
+const blockchain = new Blockchain()
+
+app.get('/api/blocks', (_req, res) => {
+  res.json(blockchain.chain)
+})
+
+app.listen(port, () => console.log(`Server is running on port ${port}`))
