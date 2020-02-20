daeeb9684a28f25fe5d28669b533dd5d7eea6953
Titux Metal <github@lgdweb.fr>
2020-02-20 12:19:21 +0100

    Update index.js file; add a post route for /api/mine

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const bodyParser = require('body-parser')
 const express = require('express')
 
 const { Blockchain } = require('./blockchain')
@@ -6,8 +7,18 @@ const { port } = require('./config')
 const app = express()
 const blockchain = new Blockchain()
 
+app.use(bodyParser.json({ extended: true }))
+
 app.get('/api/blocks', (_req, res) => {
   res.json(blockchain.chain)
+})
+
+app.post('/api/mine', (req, res) => {
+  const { data } = req.body
+
+  blockchain.addBlock({ data })
+
+  res.redirect('/api/blocks')
 })
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
