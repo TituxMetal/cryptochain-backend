c58664744560930f423966ff9bc51b9331781862
Titux Metal <github@lgdweb.fr>
2020-02-21 03:22:07 +0100

    Update config file; add the STARTING_BALANCE variable

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,6 @@
 const MINE_RATE = 1000
 const INITIAL_DIFFICULTY = 3
+const STARTING_BALANCE = 1000
 
 const defaultPort = process.env.PORT || 3000
 let peerPort
@@ -20,4 +21,10 @@ const GENESIS_DATA = {
   data: []
 }
 
-module.exports = { GENESIS_DATA, MINE_RATE, port, rootNodeAddress }
+module.exports = {
+  GENESIS_DATA,
+  STARTING_BALANCE,
+  MINE_RATE,
+  port,
+  rootNodeAddress
+}
